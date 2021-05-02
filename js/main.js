d0077d22a79744c629ee6a7dc8e94a0c740b3b3c
luca-bertero <81820809+luca-bertero@users.noreply.github.com>
2021-05-02 03:23:23 +0200

    correction

--- js/main.js
+++ js/main.js
@@ -1,7 +1,35 @@
-var number_displayed=document.getElementById("counter");
 
-var btnPlus = document.getElementById("btn-pls");
-var btnMinus = document.getElementById("btn-mns");
+//var box = document.querySelector(".counter-box");
+var ddd=document.querySelector(".counter-container");
+var qqq = document.querySelector(".calc");
+
+var box = document.createElement("div");
+ddd.appendChild(box);
+ddd.insertBefore(box, qqq);
+box.classList.add("counter-box");
+
+var counter = document.createElement("p");
+counter.innerHTML = 0;
+box.appendChild(counter);
+counter.id = "counter";
+
+var btnMinus = document.createElement("button");
+btnMinus.innerHTML="-";
+box.appendChild(btnMinus);
+btnMinus.id="ciao";
+
+var btnPlus = document.createElement("button");
+btnPlus.innerHTML="+";
+box.appendChild(btnPlus);
+
+var btnReset = document.createElement("button");
+btnReset.innerHTML="Reset";
+btnReset.id="reset-btn";
+var div = document.createElement("div");
+box.appendChild(div);
+div.appendChild(btnReset);
+
+var number_displayed = counter;
 var btn1 = document.getElementById("btn-1");
 var btn2 = document.getElementById("btn-2");
 var btn3 = document.getElementById("btn-3");
@@ -15,73 +43,88 @@ var btn0 = document.getElementById("btn-0");
 
 var number="";
 
-btnPlus.addEventListener("click",function () {
+btnPlus.onclick = add;
+
+btnReset.onclick = reset;
+
+function add(){
   number++;
   number_displayed.innerHTML=number;
-});
+}
 
-btnMinus.addEventListener("click",function () {
-  number--;
-  number_displayed.innerHTML=number;
-});
+btnMinus.onclick = minus;
 
-
-btn1.addEventListener("click",function () {
-  number=number+"1";
-  number_displayed.innerHTML=number;
-});
-
-btn2.addEventListener("click",function () {
-  number=number+"2";
-  number_displayed.innerHTML=number;
-});
-
-btn3.addEventListener("click",function () {
-  number=number+"3";
-  number_displayed.innerHTML=number;
-});
-
-btn4.addEventListener("click",function () {
-  number=number+"4";
-  number_displayed.innerHTML=number;
-});
-
-btn5.addEventListener("click",function () {
-  number=number+"5";
-  number_displayed.innerHTML=number;
-});
-
-btn6.addEventListener("click",function () {
-  number=number+"6";
-  number_displayed.innerHTML=number;
-});
-
-btn7.addEventListener("click",function () {
-  number=number+"7";
-  number_displayed.innerHTML=number;
-});
-
-btn8.addEventListener("click",function () {
-  number=number+"8";
-  number_displayed.innerHTML=number;
-});
-
-btn9.addEventListener("click",function () {
-  number=number+"9";
-  number_displayed.innerHTML=number;
-});
-
-btn0.addEventListener("click",function () {
-  number=number+"0";
-  number_displayed.innerHTML=number;
-});
-
-function minusNumber() {
+function minus() {
   number--;
   number_displayed.innerHTML=number;
 }
 
+btn1.onclick = one;
+btn2.onclick = two;
+btn3.onclick = three;
+btn4.onclick = four;
+btn5.onclick = five;
+btn6.onclick = six;
+btn7.onclick = seven;
+btn8.onclick = eight;
+btn9.onclick = nine;
+btn0.onclick = zero;
+
+function one() {
+  number = number + "1";
+  number_displayed.innerHTML = number;
+}
+
+function two() {
+  number = number + "2";
+  number_displayed.innerHTML = number;
+}
+
+function three() {
+  number = number + "3";
+  number_displayed.innerHTML = number;
+}
+
+function four() {
+  number = number + "4";
+  number_displayed.innerHTML = number;
+}
+
+function five() {
+  number = number + "5";
+  number_displayed.innerHTML = number;
+}
+
+function six() {
+  number = number + "6";
+  number_displayed.innerHTML = number;
+}
+
+function seven() {
+  number = number + "7";
+  number_displayed.innerHTML = number;
+}
+
+
+function eight() {
+  number = number + "8";
+  number_displayed.innerHTML = number;
+}
+
+
+function nine() {
+  number = number + "9";
+  number_displayed.innerHTML = number;
+}
+
+
+function zero() {
+  number = number + "0";
+  number_displayed.innerHTML = number;
+}
+
+
 function reset() {
-  number="";
-  number_displayed.innerHTML=0;
+  number = "";
+  number_displayed.innerHTML = 0;
 }
